5c5b2cda5581e479980eca6d8c60bde0de7e3b60
Kishkin Vitalii <dimpla30@gmail.com>
2023-02-02 13:24:15 +0300

    Dark Mode with React Styled Component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -20,9 +20,9 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 const Container = styled.div`
   flex: 1;
-  background-color: #202020;
+  background-color: ${({ theme }) => theme.bg};
   height: 100vh;
-  color: white;
+  color: ${({ theme }) => theme.text};
   font-size: 14px;
   position: sticky;
   top: 0;
@@ -54,7 +54,7 @@ const Item = styled.div`
 
 const Hr = styled.hr`
   margin: 15px 0px;
-  border: 0.5px solid #373737;
+  border: 0.5px solid ${({ theme }) => theme.soft};
 `;
 
 const Login = styled.div``;
@@ -72,7 +72,14 @@ const Button = styled.button`
   gap: 5px;
 `;
 
-const Menu = () => {
+const Title = styled.h2`
+  font-size: 14px;
+  font-weight: 500;
+  color: #aaaaaa;
+  margin-bottom: 20px;
+`;
+
+const Menu = ({ darkMode, setDarkMode }) => {
   return (
     <Container>
       <Wrapper>
@@ -102,12 +109,15 @@ const Menu = () => {
           History
         </Item>
         <Hr />
-        <Login>Sign in to like videos, comment and subscribe.</Login>
-        <Button>
-          <AccountCircleIcon />
-          SIGN IN
-        </Button>
+        <Login>
+          Sign in to like videos, comment and subscribe.
+          <Button>
+            <AccountCircleIcon />
+            SIGN IN
+          </Button>
+        </Login>
         <Hr />
+        <Title>BEST OF LAMATUBE</Title>
         <Item>
           <MusicVideoIcon />
           Music
@@ -145,7 +155,7 @@ const Menu = () => {
           <HelpIcon />
           Help
         </Item>
-        <Item>
+        <Item onClick={() => setDarkMode(!darkMode)}>
           <SettingsBrightnessIcon />
           Light Mode
         </Item>
