dd0fb31fe2bbc281b076fc5c1f2469cce2b3941b
Kishkin Vitalii <dimpla30@gmail.com>
2026-02-11 18:05:54 +0300

    second commit on branch feature_2

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -55,7 +55,7 @@ const Card = ({ type }) => {
         <Details type={type}>
           <ChannelImage type={type} src="https://i.pinimg.com/originals/d2/b6/d9/d2b6d9f420dd981e2387d7843e75feca.png" />
           <Texts>
-            <Title>Test Video</Title>
+            <Title>Test Video #1</Title>
             <ChannelName>Dev prod</ChannelName>
             <Info>690,999 views * 2 week ago</Info>
           </Texts>
